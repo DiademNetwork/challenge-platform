42c1658f342f7c09123a323a6aaa3a633dd81309
sevenflash <sevenflash@yandex.ru>
2018-08-01 14:05:57 +0300

    update contract

--- contracts/ChallengeToken.sol
+++ contracts/ChallengeToken.sol
@@ -7,6 +7,7 @@ contract ChallengeToken is ERC721Token, Ownable {
     mapping (uint256 => bool) public isCommunityChallenge;
     mapping (uint256 => string) public titles;
     mapping (uint256 => mapping (address => uint256)) public rewards;
+    mapping (uint256 => address[]) public verifiers;
     mapping (uint256 => mapping (address => bool)) public confirmations;
     mapping (uint256 => uint256) public totalRewards;
 
@@ -21,7 +22,7 @@ contract ChallengeToken is ERC721Token, Ownable {
         BoughtToken(msg.sender, index);
     }
 
-    function createWithReward(string _title, bool _isCommunityChallenge, uint256 _amount, address _verifier) payable {
+    function createWithReward(string _title, bool _isCommunityChallenge, uint256 _amount, address _verifier) payable external {
         require(msg.value == _amount);
         require(confirmations[index][_verifier] == false);
 
@@ -31,28 +32,38 @@ contract ChallengeToken is ERC721Token, Ownable {
 
         titles[index] = _title;
 
-        rewards[index][_verifier] += msg.value;
         totalRewards[index] += msg.value;
+
+        if (rewards[index][_verifier] == 0) {
+            verifiers[index].push(_verifier);
+            rewards[index][_verifier] = msg.value;
+        } else {
+            rewards[index][_verifier] += msg.value;
+        }
 
         isCommunityChallenge[index] = _isCommunityChallenge;
 
         BoughtToken(msg.sender, index);
     }
 
-    function confirm(uint256 _index) {
+    function confirm(uint256 _index) external {
         confirmations[_index][msg.sender] = true;
     }
 
-    function addReward(uint256 _index, uint256 _amount, address _verifier) payable {
+    function addReward(uint256 _index, uint256 _amount, address _verifier) payable external {
         require(msg.value == _amount);
 
         require(confirmations[_index][_verifier] == false);
 
-        rewards[_index][_verifier] += _amount;
-        totalRewards[_index] += _amount;
+        if (rewards[_index][_verifier] == 0) {
+            verifiers[_index].push(_verifier);
+            rewards[_index][_verifier] = msg.value;
+        } else {
+            rewards[_index][_verifier] += msg.value;
+        }
     }
 
-    function claimReward(uint256 _index, address _verifier) {
+    function claimReward(uint256 _index, address _verifier) external {
         require(ownerOf(_index) == msg.sender);
         require(confirmations[_index][_verifier] == true);
 
@@ -64,8 +75,13 @@ contract ChallengeToken is ERC721Token, Ownable {
         msg.sender.transfer(reward);
     }
 
-    function acceptChallenge(uint256 _index) {
-        ChallengeAccepted(msg.sender, _index);
+    function acceptChallenge(uint256 _index) external returns (bool) {
+        if (isCommunityChallenge[_index] == true) {
+            ChallengeAccepted(msg.sender, _index);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     function myTokens() external view returns (uint256[])
@@ -74,11 +90,12 @@ contract ChallengeToken is ERC721Token, Ownable {
     }
 
     function getToken(uint256 _tokenId) external view
-        returns (bool tokenType_, string tokenTitle_, uint256 tokenReward_)
+        returns (bool tokenType_, string tokenTitle_, uint256 tokenReward_, address[] verifiers_)
     {
         tokenType_ = isCommunityChallenge[_tokenId];
         tokenTitle_ = titles[_tokenId];
         tokenReward_ = totalRewards[_tokenId];
+        verifiers_ = verifiers[_tokenId];
     }
 
     function ChallengeToken() ERC721Token("Challenge Token", "CDO") public {
